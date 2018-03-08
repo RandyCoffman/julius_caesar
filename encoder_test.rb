c5fe0241255f5d90474f9d360d6c616fc1fb5167
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-08 09:01:54 -0500

    Added more tests to test for the 8th day of the month

--- encoder_test.rb
+++ encoder_test.rb
@@ -12,22 +12,18 @@ class Caesar_cipher < Minitest::Test
 
 	def test_string_encode
 		assert_equal("Fi wyvi xs hvmro csyv Szepxmri", caesar_cipher("Be sure to drink your Ovaltine", 4))
-		p caesar_cipher("Be sure to drink your Ovaltine", 4)
 	end
 
 	def test_string_decode
 		assert_equal("Be sure to drink your Ovaltine", reverse_caesar_cipher("Fi wyvi xs hvmro csyv Szepxmri", 4))
-		p reverse_caesar_cipher("Fi wyvi xs hvmro csyv Szepxmri", 4)
 	end
 
 	def test_string_7th_day_encode
-		assert_equal("Il zbyl av kypur fvby Vchsapul", caesar_cipher("Be sure to drink your Ovaltine"))
-		p caesar_cipher("Be sure to drink your Ovaltine")
+		assert_equal("Il zbyl av kypur fvby Vchsapul", caesar_cipher("Be sure to drink your Ovaltine", 7))
 	end
 
 	def test_string_7th_day_decode
-		assert_equal("Be sure to drink your Ovaltine", reverse_caesar_cipher("Il zbyl av kypur fvby Vchsapul"))
-		p reverse_caesar_cipher("Fi wyvi xs hvmro csyv Szepxmri")
+		assert_equal("Be sure to drink your Ovaltine", reverse_caesar_cipher("Il zbyl av kypur fvby Vchsapul", 7))
 	end
 
 	def test_multi_string_encode
@@ -35,6 +31,14 @@ class Caesar_cipher < Minitest::Test
 	end
 
 	def test_multi_string_decode
-		assert_equal("Be sure to drink your Ovaltine, hello", reverse_caesar_cipher(["Il zbyl av kypur fvby Vchsapul", "olssv"].join(", ")))
+		assert_equal("Be sure to drink your Ovaltine, hello", reverse_caesar_cipher(["Il zbyl av kypur fvby Vchsapul", "olssv"].join(", "), 7))
+	end
+
+	def test_string_8th_day_encode
+		assert_equal("Jm aczm bw lzqvs gwcz Wditbqvm", caesar_cipher("Be sure to drink your Ovaltine"))
+	end
+
+	def test_string_8th_day_decode
+		assert_equal("Be sure to drink your Ovaltine", reverse_caesar_cipher("Jm aczm bw lzqvs gwcz Wditbqvm"))
 	end
 end
